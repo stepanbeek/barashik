f65fee67ad42c730488a6ee408228b821581462c
stepanbeek <157492184+stepanbeek@users.noreply.github.com>
2024-02-02 11:39:03 +0300

    Практическая работа 2. 02.02.2024

---
+++ main (9).py
@@ -0,0 +1,16 @@
+a = 19
+b = 7
+g = (a<11) and (b != 7)
+print(g)
+m = (a == 19) and (b >= 8) 
+print (m)
+h = (a <= 12) and (b == 8) 
+print(h)
+q = (a != 34) and (b != 12)
+print(q)
+l = (a!= 34) or (b <= 15)
+print(l)
+str1 = "a"
+str2 = "b"
+momo = str1 < "c" and str2 < "c"
+print(momo)
