4a2f772fb3faf2e478f8b9d990500d666a1d92f4
stepanbeek <157492184+stepanbeek@users.noreply.github.com>
2024-02-02 11:04:56 +0300

    Практическая работа 02.02.2024

---
+++ main(9).py
@@ -0,0 +1,3 @@
+tseloe = 10drobnoe = 8.4stroka = "No"da = "yes"lodka = "_"big_netseloe = tseloe * 3.5drobnoe = drobnoe - 1print(tseloe // drobnoe)
+print(big_netseloe // drobnoe)
+stroka = stroka + stroka + lodka + da * 3print(big_netseloe, drobnoe, stroka)
