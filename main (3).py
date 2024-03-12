7bab94752b6c143a4f3ca0c4e8511019be4cae1b
stepanbeek <157492184+stepanbeek@users.noreply.github.com>
2024-03-12 15:45:21 +0300

    Классная работа 12.03.2024

--- main (3).py
+++ main (3).py
@@ -1,37 +1,46 @@
-#Задача 1
-n = int(input("Введите натуральное число n: "))
-
-i = 1
-while i < n:
-    print(i)
-    i += 1
-
+#задача 1
+try:
+    n = int(input("Введите размерность одномерного массива: "))
+    if n <= 0:
+        raise ValueError("Размерность массива должна быть положительным числом.")
+    
+    a = [i+1 for i in range(n)] 
+    print("Исходный массив:", arr)
+    
+    b = [x**2 for x in arr]
+    print("Квадраты элементов массива:", squared_arr)
+    
+    c = [x**3 for x in arr]
+    print("Кубы элементов массива:", cubed_arr)
+    
+except ValueError as e:
+    print(f"Ошибка: {e}")
 #Задача 2
-a = float(input("Введите вещественное число a: "))
-n = 1
-sum = 0
-while sum <= a:
-    sum += 1/n
-    if sum > a:
-        break
-    print(n)
-    n += 1
+def find_minimum(a, b, c):
+    return min(a, b, c)
 
-#Задача 3
-n = int(input("Введите число n: "))
-i = 1
-while i*i <= n:
-    i += 1
-print("Первое натуральное число, квадрат которого больше", n, ":", i)
+go1 = int(input("Введите первое число: "))
+go2 = int(input("Введите второе число: "))
+go3 = int(input("Введите третье число: "))
 
+result = minimum(go1, go2, go3)
+print("Наименьшее число: ", result)
+#Задача 3 
+a = input("Введите символ: ")
+b = int(input("Введите целое число: "))
+
+c = ord(a) % b
+print("Остаток от деления кода символа на число:", c)
 #Задача 4
-n = 201  
-while n % 12 != 0:
-    n += 1
-print("Минимальное число больше 200, которое делится нацело на 12:", n)
+input_symbol = input("Введите символ: ")
+input_number = float(input("Введите вещественное число: "))
+result = int(input_number) // ord(input_symbol)
+print("Целая часть от деления числа на код символа:", result)
+#задача 5
+x1, y1 = map(float, input("Введите координаты вершины A (x y): ").split())
+x2, y2 = map(float, input("Введите координаты вершины B (x y): ").split())
+x3, y3 = map(float, input("Введите координаты вершины C (x y): ").split())
 
-#Задача 5
-n = 600 
-while n % 28 != 0:
-    n -= 1
-print("Максимальное число не превышающее 600, которое делится нацело на 28:", n)
+a = abs((x1*(y2-y3) + x2*(y3-y1) + x3*(y1-y2)) / 2)
+
+print("Площадь треугольника:", a)
